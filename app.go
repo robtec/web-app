90ae2b24f997f6ae862c2c0aed39753b72dd45bd
robtec <rob.p.tec@gmail.com>
2023-11-23 18:24:30 +0000

    added

--- app.go
+++ app.go
@@ -3,22 +3,18 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	r := chi.NewRouter()
+	r := gin.Default()
 
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
 	})
 
-	http.ListenAndServe(":8080", r)
+	r.Run()
 }

849a6cba79881956165229753c2e8b178dd6b519
robtec <rob.p.tec@gmail.com>
2023-11-25 02:22:08 +0000

    added

--- app.go
+++ app.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
+
+var conn *pgx.Conn
 
 func main() {
 
+	var err error
+
 	r := gin.Default()
+
+	conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connection to database: %v\n", err)
+		os.Exit(1)
+	}
 
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "hi")

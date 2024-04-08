6592014ac8a3fb97232769b70009c8cfcfc56508
Rob <robtec@users.noreply.github.com>
2024-04-08 23:04:27 +0100

    Update app.go

--- app.go
+++ app.go
@@ -7,23 +7,11 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5"
 )
-
-var conn *pgx.Conn
 
 func main() {
 
-	var err error
-
 	r := gin.Default()
-
-	conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connection to database: %v\n", err)
-		os.Exit(1)
-	}
 
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, conn.Config().Host)

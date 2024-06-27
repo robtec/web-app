c8a8239ce7f4635b0a7b0a3349d468f7c1b496a0
Rob <rob.p.tec@gmail.com>
2024-06-27 22:32:21 +0100

    added

--- router/server.go
+++ router/server.go
@@ -4,12 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/gocolly/colly/v2"
 )
 
 func RunHTTPServer(ctx context.Context, port string) (err error) {
@@ -18,27 +15,12 @@ func RunHTTPServer(ctx context.Context, port string) (err error) {
 
 	tm := time.Now()
 
-	var msg = "all good"
-
-	cly := colly.NewCollector(
-		colly.UserAgent("Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"),
-		colly.AllowURLRevisit(),
-	)
-
-	cly.OnHTML("footer", func(e *colly.HTMLElement) {
-		msg = strings.Replace(e.Text, "\n", "", -1)
-	})
-
-	cly.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	router.LoadHTMLGlob("templates/*")
+	router.GET("/youtube", Youtube)
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
-			"time":  fmt.Sprintf("%s", tm),
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ping",
 		})
 	})
 
@@ -49,22 +31,6 @@ func RunHTTPServer(ctx context.Context, port string) (err error) {
 	router.GET("/pong", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "ping",
-		})
-	})
-
-	router.GET("/scrape", func(c *gin.Context) {
-
-		urlQ := c.Query("url")
-
-		err := cly.Visit(urlQ)
-
-		if err != nil {
-			msg = err.Error()
-		}
-
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": msg,
-			"time":  fmt.Sprintf("%s", tm),
 		})
 	})
 
---
+++ router/youtube.go
@@ -0,0 +1,105 @@
+package router
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Kind  string  `json:"kind"`
+	Items []Items `json:"items"`
+}
+
+type Items struct {
+	Kind  string `json:"kind"`
+	Id    string `json:"id"`
+	Stats Stats  `json:"statistics"`
+}
+
+type Stats struct {
+	Views       string `json:"viewCount"`
+	Subscribers string `json:"subscriberCount"`
+	Videos      string `json:"videoCount"`
+}
+
+func Youtube(c *gin.Context) {
+
+	var response Response
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://www.googleapis.com/youtube/v3/channels", nil)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if _, ok := c.GetQueryArray("key"); !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"missing parameter": "key",
+		})
+		return
+	}
+
+	if _, ok := c.GetQueryArray("id"); !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"missing parameter": "id",
+		})
+		return
+	}
+
+	q := req.URL.Query()
+
+	q.Add("key", c.Query("key"))
+	q.Add("id", c.Query("id"))
+	q.Add("part", "statistics")
+
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println("Response Status: ", resp.Status)
+
+	body, _ := io.ReadAll(resp.Body)
+
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	resultCode := resp.StatusCode
+
+	if resultCode >= 300 || resultCode <= 199 {
+		c.JSON(resultCode, response)
+		return
+	}
+
+	numberStr := response.Items[0].Stats.Subscribers
+
+	number, _ := strconv.Atoi(numberStr)
+
+	c.JSON(http.StatusOK, gin.H{
+		"number": number,
+	})
+}
